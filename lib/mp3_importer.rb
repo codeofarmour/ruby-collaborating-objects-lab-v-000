a58c731110a9fcde71651b79bd0d51e1a9940104
Terri Riggle <codeofarmour@outlook.com>
2018-07-10 17:10:08 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,4 +6,12 @@ class MP3Importer
   def initialize(path)
     @path = path 
   end
+  
+  def files
+    files = Dir.entries(@path).select {|file| file.end_with?('mp3')}
+  end
+  
+  def MP3Import
+    self.files.each {|file| Song.new_by_filename(file)}
+  end 
 end
